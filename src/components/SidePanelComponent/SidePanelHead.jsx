a6adc776ac6d04b2d104f98091c0a4279a34bd05
akash-panthri <akashpanthri2000@gmail.com>
2024-03-22 13:46:25 +0530

    changed font size for head in sizebar again

--- src/components/SidePanelComponent/SidePanelHead.jsx
+++ src/components/SidePanelComponent/SidePanelHead.jsx
@@ -5,8 +5,8 @@ import { GoBell } from "react-icons/go";
 export default function SidePanelHead() {
   return (
     <div className="h-[7.5%] border-b  flex flex-row items-center justify-between">
-        <div className="flex flex-row w-2/4 justify-between mx-2">
-          <HiUser className='text-2xl sm:text-base rounded-full' />
+        <div className="text-2xl sm:text-base flex flex-row w-2/4 justify-between mx-2">
+          <HiUser className=' rounded-full' />
           <span>Hello User</span>
         </div>
         <span className="font-bold px-5 text-3xl">
