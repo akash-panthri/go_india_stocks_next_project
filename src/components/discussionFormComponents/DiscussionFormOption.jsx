34124c8b533b29a758fca8c7b5305e33d13a201b
akash-panthri <akashpanthri2000@gmail.com>
2024-03-22 13:38:39 +0530

    changed font size for head in sizebar

--- src/components/discussionFormComponents/DiscussionFormOption.jsx
+++ src/components/discussionFormComponents/DiscussionFormOption.jsx
@@ -6,7 +6,7 @@ import OptionHead from './OptionHead'
 
 export default function DiscussionFormOption({discussionFormElement}) {
   return (
-    <div className='shadow-xl bg-gray-100 py-3 mb-11 px-3 sm:px-5 sm:py-3 mt-3 text-xs sm:text-2xl' >
+    <div className='shadow-xl bg-gray-100 py-3 mb-11 px-3 sm:px-5 sm:py-3 mt-3 text-xs sm:text-lg' >
     <OptionHead discussionFormElement={discussionFormElement} />
     <div className='mx-3 sm:mx-40 px-4 sm:px-4 mb-5'>{discussionFormElement.post_text}
 
